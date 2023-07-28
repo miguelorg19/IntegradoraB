5b28ab1e3a6b3764ebd8d3a77c7ea2773145e7b3
miguelorg19 <139198305+miguelorg19@users.noreply.github.com>
2023-07-27 22:27:46 -0600

    Imagen de usuario
    Cargar img de usuario

--- Integradora/src/Modelos/imagenes.php
+++ Integradora/src/Modelos/imagenes.php
@@ -11,4 +11,20 @@ class imagenes{
         $base64 = "data:{$tipo};base64,{$bas64}";
         return $base64;
     }
+
+    public function obtenerimaus($nombreimg){
+        $dir = __DIR__.'/../../public/Usuarios/';
+        $path = "{$dir}{$nombreimg}";
+        $data = file_get_contents($path);
+        $bas64 = base64_encode($data);
+        $tipo = mime_content_type($path);
+        $base64 = "data:{$tipo};base64,{$bas64}";
+        return $base64;
+    }
+    public function verfoto($idUs)
+    {
+        $sql = $conexion->prepare("SELECT Foto from usuarios where ID_Usuario = ?");
+        $foto = $sql = execute([$idUs]);
+        return $foto;
+    }
 }
